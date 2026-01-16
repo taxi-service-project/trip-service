5d729acf3f53178fd1bca4b5b9d1b003b9330b7a
201912025 <michi2012@naver.com>
2026-01-16 13:13:51 +0900

    traceId 카프카 관측성 on 설정 및 스레드 전환 구간에서 확실한 전파 위해 Hooks 설정 명시적으로 추가

--- src/main/java/com/example/trip_service/TripServiceApplication.java
+++ src/main/java/com/example/trip_service/TripServiceApplication.java
@@ -3,12 +3,15 @@ package com.example.trip_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TripServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(TripServiceApplication.class, args);
 	}
 
