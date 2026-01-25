34ec296c427468bd9d8bd7bdd2f942b1c63422f4
201912025 <michi2012@naver.com>
2026-01-25 17:45:34 +0900

    feat: 애플리케이션 종료 요청 시, 큐에 남아있는 작업과 진행 중인 작업이 완료될 때까지 대기하도록 Executor 설정 추가

--- src/main/java/com/example/trip_service/config/AsyncConfig.java
+++ src/main/java/com/example/trip_service/config/AsyncConfig.java
@@ -20,6 +20,11 @@ public class AsyncConfig {
         executor.setMaxPoolSize(50);
         executor.setQueueCapacity(1000);
         executor.setThreadNamePrefix("Trip-Relay-");
+
+        // 애플리케이션 종료 요청 시, 큐에 남아있는 작업과 진행 중인 작업이 완료될 때까지 대기함
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        // 60초가 지나도 안 끝나면 그때는 강제 종료 (무한 대기 방지)
+        executor.setAwaitTerminationSeconds(60);
         executor.initialize();
         return executor;
     }
