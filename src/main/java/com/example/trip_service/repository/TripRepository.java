85de02784dd42aeb2913d6f6eaf07ef734905a12
201912025 <michi2012@naver.com>
2026-01-25 18:36:52 +0900

    feat: 너무 긴 락 대기로 인한 커넥션 풀 고갈로 인한 장애 전파 되서 다른 가능한 기능 까지 마비되는 것을 막기 위해 락 타임아웃 3초 설정

--- src/main/java/com/example/trip_service/repository/TripRepository.java
+++ src/main/java/com/example/trip_service/repository/TripRepository.java
@@ -2,9 +2,11 @@ package com.example.trip_service.repository;
 
 import com.example.trip_service.entity.Trip;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -13,6 +15,7 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     Optional<Trip> findByTripId(String tripId);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
     @Query("select t from Trip t where t.tripId = :tripId")
     Optional<Trip> findByTripIdWithLock(@Param("tripId") String tripId);
 
