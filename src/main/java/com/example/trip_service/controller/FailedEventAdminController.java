974142ab19c8340ebe0638935bfc538d7cabfce0
201912025 <michi2012@naver.com>
2026-02-03 11:51:28 +0900

    > feat: dlt 에러테이블 이벤트들 재발급 api 서비스별 구분 위해 매핑앞 api/서비스명 추가 >

--- src/main/java/com/example/trip_service/controller/FailedEventAdminController.java
+++ src/main/java/com/example/trip_service/controller/FailedEventAdminController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin/failed-events")
+@RequestMapping("/api/trips/admin/failed-events")
 @RequiredArgsConstructor
 public class FailedEventAdminController {
 
